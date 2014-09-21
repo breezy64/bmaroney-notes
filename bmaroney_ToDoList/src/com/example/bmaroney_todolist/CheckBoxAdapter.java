2a51df9e094499b92f02a0c12b6356016e7cb32a
Byron Maroney <bmaroney@ualberta.ca>
2014-09-21 17:28:50 -0600

    Context menu is sane
    Signed-off-by: Byron Maroney <bmaroney@ualberta.ca>

---
+++ bmaroney_ToDoList/src/com/example/bmaroney_todolist/CheckBoxAdapter.java
@@ -0,0 +1,29 @@
+package com.example.bmaroney_todolist;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+public class CheckBoxAdapter extends ArrayAdapter<ToDoListItem> {
+	private LayoutInflater mInflater;
+    private int mViewResourceId;
+    
+	public CheckBoxAdapter(Context context, int textViewResourceId) {
+		super(context, textViewResourceId);
+		mViewResourceId = textViewResourceId;
+		mInflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+	}    
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		convertView = mInflater.inflate(mViewResourceId, null);
+	    CheckBox cb = (CheckBox)convertView.findViewById(R.id.checkBox1);
+	    cb.setChecked(getItem(position).toDoCompleted());
+	    TextView tv = (TextView)convertView.findViewById(R.id.textView1);
+	    tv.setText(getItem(position).toString());
+	    return convertView;
+	}
+}
